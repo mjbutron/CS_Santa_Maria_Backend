bf71c4578db5a51570fc8329d8b9856b3c3a74c3
mjbutron <butron87@gmail.com>
2020-02-04 16:31:12 +0100

    Modificaciones en talleres y pre modal en dashboard

--- src/app/components/admin/workshop/workshop.component.ts
+++ src/app/components/admin/workshop/workshop.component.ts
@@ -14,7 +14,12 @@ import { WorkshopInterface } from 'src/app/models/workshop-interface';
 })
 export class WorkshopComponent implements OnInit {
 
+  // Path
+  path = "http://localhost/apiRest/uploads/";
+  // Workshops
+  workShopObj: WorkshopInterface;
   workShops: WorkshopInterface[] = [];
+  inHomeChk: boolean;
   // Errors
   errors = "";
   // Numeros páginas
@@ -29,10 +34,20 @@ export class WorkshopComponent implements OnInit {
   private numResults: number = 10;
   // Scroll
   @ViewChild("subsScroll", { static: true }) subsScrollDiv: ElementRef;
+  // Scroll Form
+  @ViewChild("editWorkshop", { static: true }) editWorkshop: ElementRef;
+  // Form
+  activeForm = false;
+  populateForm = false;
 
-  constructor(private dataApi: DataApiService, public toastr: ToastrService) { }
+  constructor(private dataApi: DataApiService, public toastr: ToastrService) {
+    this.workShopObj = new WorkshopInterface();
+    this.inHomeChk = false;
+  }
 
   ngOnInit() {
+    this.activeForm = false;
+    this.populateForm = false;
     this.scrollToDiv();
     this.getWorkShopsByPage(this.page);
   }
@@ -60,7 +75,38 @@ export class WorkshopComponent implements OnInit {
     });
   }
 
+  onEditWorkshop(workShop: WorkshopInterface) {
+    // Habilitar form en formato eedición
+    this.activeForm = true;
+    // Setear valores al ui
+    this.workShopObj.id = workShop.id;
+    this.workShopObj.home = workShop.home;
+    this.inHomeChk = (workShop.home == 1) ? true : false;
+    this.workShopObj.title = workShop.title;
+    this.workShopObj.description_home = workShop.description_home;
+    this.workShopObj.image = workShop.image;
+    this.workShopObj.subtitle = workShop.subtitle;
+    this.workShopObj.price = workShop.price;
+    this.workShopObj.address = workShop.address;
+    this.workShopObj.session_date = workShop.session_date;
+    console.log(workShop.session_date);
+
+    this.workShopObj.session_start = workShop.session_start;
+    this.workShopObj.session_end = workShop.session_end;
+    this.workShopObj.sessions = workShop.sessions;
+    this.workShopObj.description = workShop.description;
+    this.workShopObj.user_id = workShop.user_id;
+    setTimeout (() => {
+         // Mover el scroll al form
+         this.scrollToForm();
+      }, 200);
+  }
+
   scrollToDiv() {
       this.subsScrollDiv.nativeElement.scrollIntoView(true);
+  }
+
+  scrollToForm() {
+      this.editWorkshop.nativeElement.scrollIntoView({behavior:"smooth"});
   }
 }
