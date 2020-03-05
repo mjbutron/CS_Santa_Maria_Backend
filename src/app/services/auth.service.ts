73a5bfb81dddb37bdad2292d4d22943de5a26e33
mjbutron <butron87@gmail.com>
2020-03-05 20:58:56 +0100

    Servicio de autenticación

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,10 +5,57 @@ import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
 import { isNullOrUndefined } from 'util';
 
+import { UserInterface } from 'src/app/models/user-interface';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private url = environment.urlApiRest;
+  userToken: string;
+
+  constructor(private http: HttpClient) {
+    this.getTokenStorage();
+  }
+
+  login( user: UserInterface){
+    const authData = {
+      ...user,
+      returnSecureToken: true
+    };
+
+    return this.http.post(`${this.url}/login`,
+      authData).pipe(
+        map(res => {
+          this.saveToken(res['token']);
+          return res;
+        })
+      );
+  }
+
+  private saveToken(idToken: string){
+    this.userToken = idToken;
+    localStorage.setItem('accessTkn', idToken);
+  }
+
+  getTokenStorage(){
+    if(localStorage.getItem('accessTkn')){
+      this.userToken = localStorage.getItem('accessTkn');
+    }else{
+      this.userToken = '';
+    }
+    return this.userToken;
+  }
+
+  isAuthenticated(): boolean {
+    return this.userToken.length > 10;
+  }
+
+  logout(): boolean {
+    localStorage.removeItem('accessTkn');
+    this.userToken = '';
+    return (this.userToken == '') ? true : false;
+  }
+
 }
