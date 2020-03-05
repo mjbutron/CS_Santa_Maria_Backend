c57f9cb58ed5691c10b4ce52f1b64d34ef9b73dd
mjbutron <butron87@gmail.com>
2020-03-05 20:56:10 +0100

    Guard e interfaz de usuario

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/services/auth.service';
+import { Globals } from 'src/app/common/globals';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  globals: Globals;
+
+  constructor(private auth: AuthService, private router: Router, globals: Globals) {
+    this.globals = globals;
+  }
+
+  canActivate(): boolean {
+    if(this.auth.isAuthenticated()){
+      this.globals.isAuth = true;
+      return true;
+    }else{
+      this.globals.isAuth = false;
+      this.router.navigateByUrl('/');
+      return false;
+    }
+  }
+
+}
