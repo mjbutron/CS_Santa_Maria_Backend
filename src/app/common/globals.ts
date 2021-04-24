8be2571af13c44ffa0a6f9151bee914e9229222d
mjbutron <butron87@gmail.com>
2021-04-24 17:47:11 +0200

    Cambios en global y nombres en iconos.

--- src/app/common/globals.ts
+++ src/app/common/globals.ts
@@ -3,10 +3,15 @@ import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class Globals {
+  // URLs
+  imgRootPath: string = environment.imageRootPath;
+  // User image profile (Default)
+  userImage: string = 'default-avatar.png';
+  // User name and rol (Default)
   rol: string = 'rolDummy';
   user: string = 'userDummy';
-  imgRootPath: string = environment.imageRootPath;
-  userImage: string = 'default-avatar.png';
+  // Show if you are logged in
   isAuth: boolean = false;
+  // Sets whether the password has changed
   isChangePass: boolean = true;
 }
