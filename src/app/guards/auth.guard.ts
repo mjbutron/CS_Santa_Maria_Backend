351d067f8e395e14dcd2fad488cad7bd94947f07
mjbutron <butron87@gmail.com>
2021-12-23 13:06:59 +0100

    Comentarios en servicio de autenticación

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { Router } from '@angular/router';
-
-import { AuthService } from 'src/app/services/auth.service';
 import { Globals } from 'src/app/common/globals';
+// Services
+import { AuthService } from 'src/app/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-
+  // Globals
   globals: Globals;
 
+  /**
+   * Constructor
+   * @param auth     Authentication service
+   * @param router   Router module
+   * @param globals  Globals
+   */
   constructor(private auth: AuthService, private router: Router, globals: Globals) {
     this.globals = globals;
   }
 
+  /**
+   * Allow or deny access to a URL
+   * @return True if allowed and False if denied
+   */
   canActivate(): boolean {
-    if(this.auth.isAuthenticated()){
+    if (this.auth.isAuthenticated()) {
       this.globals.isAuth = true;
       return true;
-    }else{
+    } else {
       this.globals.isAuth = false;
       this.router.navigateByUrl('login');
       return false;
